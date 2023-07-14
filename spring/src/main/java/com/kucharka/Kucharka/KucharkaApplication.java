9d147ca595c8ac46ea87f1e8a7481096edab1a85
Kuric Christián <christian.kuric@gmail.com>
2023-07-14 15:22:14 +0200

    project setup

---
+++ spring/src/main/java/com/kucharka/Kucharka/KucharkaApplication.java
@@ -0,0 +1,13 @@
+package com.kucharka.Kucharka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KucharkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KucharkaApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/kucharka/Kucharka/KucharkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kucharka.Kucharka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KucharkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
