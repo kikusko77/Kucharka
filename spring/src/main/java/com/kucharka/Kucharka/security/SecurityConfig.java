1499d48cc0d77e5dc16dbd247e1f056fcd30dca5
kikusko77 <christian.kuric@gmail.com>
2023-08-21 13:11:00 +0200

    added cors

--- spring/src/main/java/com/kucharka/Kucharka/security/SecurityConfig.java
+++ spring/src/main/java/com/kucharka/Kucharka/security/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.kucharka.Kucharka.security;
 import com.kucharka.Kucharka.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import com.kucharka.Kucharka.security.filter.AuthenticationFilter;
@@ -12,6 +13,11 @@ import com.kucharka.Kucharka.security.filter.JWTAuthorizationFilter;
 import com.kucharka.Kucharka.security.manager.CustomAuthenticationManager;
 import lombok.AllArgsConstructor;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -20,6 +26,19 @@ public class SecurityConfig {
 
     private final CustomAuthenticationManager customAuthenticationManager;
     private final UserService userService;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080")); // Replace with your allowed origins
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 
 
@@ -30,9 +49,13 @@ public class SecurityConfig {
         http
 //                .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
 //                .and()
+                .cors()
+                .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/admin/**","/groceries/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/admin/**", "/groceries/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/admin/**", "/groceries/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/admin/**", "/groceries/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
