976fba978dde64bc5bc816a616bd902fa0f5e524
kikusko77 <christian.kuric@gmail.com>
2023-08-22 22:31:25 +0200

    user fix

--- spring/src/main/java/com/kucharka/Kucharka/DTO/UserDTO.java
+++ spring/src/main/java/com/kucharka/Kucharka/DTO/UserDTO.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class UserDTO {
     private Long id;
     private String name;
+    private String password;
     private Role role;
     private List<RecipeDTO> recipes;
 }
